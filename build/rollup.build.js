e4a3f3152cc3d59d278d2184847cec5c690900c1
chenzhijie <chenzhijie@shuwen.com>
2022-05-23 14:02:19 +0800

    feat:配置第三方库不打包，而是使用外部依赖

--- build/rollup.build.js
+++ build/rollup.build.js
@@ -5,6 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
+  external: ['react', 'react-dom'], // 配置第三方库不打包，而是使用外部依赖
   input: 'src/index.jsx',
   output: {
     file: 'example/dist/bundle.js',
